0767b795cdd751bb4f4b8d765a52d0988ba0e352
Akvile4 <aqua0818@gmail.com>
2021-12-31 16:24:42 +0200

    uncommented

--- functions.js
+++ functions.js
@@ -77,118 +77,118 @@
 // // we call the second function with the argument and with template literals we print the answer out
 // console.log(`The temperature is ${getFahrenheit(15)} F.`);
 
-// // Activity 1
-// // Declaration (another way of writing the fuction) 
-// // creating a function with one parameter
-// function factorial (n) {
-//     // if our parameter meets one of the conditions then return 1
-//     if ((n === 0) || (n === 1)) {
-//         return 1;
-//     }
-//     // if none of the conditions were met do this
-//     else {
-//         return (n * factorial(n-1));
-//     }
-// };
+// Activity 1
+// Declaration (another way of writing the fuction) 
+// creating a function with one parameter
+function factorial (n) {
+    // if our parameter meets one of the conditions then return 1
+    if ((n === 0) || (n === 1)) {
+        return 1;
+    }
+    // if none of the conditions were met do this
+    else {
+        return (n * factorial(n-1));
+    }
+};
 
-// // calling the function with one argument and asking to print that out 
-// console.log(factorial(33));
+// calling the function with one argument and asking to print that out 
+console.log(factorial(33));
 
-// // making the same function just in an Arrow function
-// const factorial = (n) => {
-//     if ((n === 0) || (n ===1)) {
-//         return 1;
-//     }
-//     else {
-//         return (n * factorial(n-1));
-//     }
-// };
+// making the same function just in an Arrow function
+const factorial = (n) => {
+    if ((n === 0) || (n ===1)) {
+        return 1;
+    }
+    else {
+        return (n * factorial(n-1));
+    }
+};
 
-// console.log(factorial(33));
+console.log(factorial(33));
 
-// // Activity 2
-// // 1 WAY
-// // we create a variable 
-// let orderCount = 0;
+// Activity 2
+// 1 WAY
+// we create a variable 
+let orderCount = 0;
 
-// // we create an arrow function with two parameters
-// const takeOrder = (topping, topping2) => {
+// we create an arrow function with two parameters
+const takeOrder = (topping, topping2) => {
 
-//     // with temmplate literals we print out our parameters
-//     console.log(`Pizza with ${topping} and ${topping2}`);
-//     // to our variable we add +1
-//     orderCount++;
-// }
+    // with temmplate literals we print out our parameters
+    console.log(`Pizza with ${topping} and ${topping2}`);
+    // to our variable we add +1
+    orderCount++;
+}
 
-// // calling the functions and asking them to print the results
-// takeOrder('pineapples','mushrooms');
-// takeOrder('tomatoes','ham');
+// calling the functions and asking them to print the results
+takeOrder('pineapples','mushrooms');
+takeOrder('tomatoes','ham');
 
-// // checking how many orders we already placed
-// console.log(orderCount);
+// checking how many orders we already placed
+console.log(orderCount);
 
-// // 2 WAY
-// // we create a variable 
-// let orderCount = 0;
+// 2 WAY
+// we create a variable 
+let orderCount = 0;
 
-// // we create an arrow function with two parameters
-// const takeOrder = (topping1, topping2) => {
-//     // when we run the function it will add to the orderCount +1
-//     orderCount++;
-//     // it will print out the order's number
-//     console.log(`Order number ${orderCount}:`);
-//     // it will print out the toppings with template literals
-//     console.log(`Pizza with ${topping1} and ${topping2}.`);
-// };
+// we create an arrow function with two parameters
+const takeOrder = (topping1, topping2) => {
+    // when we run the function it will add to the orderCount +1
+    orderCount++;
+    // it will print out the order's number
+    console.log(`Order number ${orderCount}:`);
+    // it will print out the toppings with template literals
+    console.log(`Pizza with ${topping1} and ${topping2}.`);
+};
 
-// // we run the function twice with different arguments
-// takeOrder('mushrooms','ham');
-// takeOrder('pineapples','chorizo');
+// we run the function twice with different arguments
+takeOrder('mushrooms','ham');
+takeOrder('pineapples','chorizo');
 
 
-// // Activity 3
-// // we create a constant of the right PIN number
-// const pin1 = 1234;
-// // we create a variable of guessing the pin number
-// let pin2 = 1234;
-// // we create a variable of how much money is in the bank account
-// let cashIn = 1200;
-// // we create a variable of how much money we want to take out 
-// let cashOut = 1200;
+// Activity 3
+// we create a constant of the right PIN number
+const pin1 = 1234;
+// we create a variable of guessing the pin number
+let pin2 = 1234;
+// we create a variable of how much money is in the bank account
+let cashIn = 1200;
+// we create a variable of how much money we want to take out 
+let cashOut = 1200;
 
-// // creating an arrow function to check if the pin is right with two parameters
-// const checkPin = (pinNumber, pinNum) => {
-//     // if the both pin numbers match and if yes return true
-//     if (pinNumber === pinNum) {
-//         return true;
-//     }
-//     // if the pin we guessed is wrong then it will print out this
-//     else {
-//         console.log('You PIN is wrong !!! Try again !');
-//     }
-// }
+// creating an arrow function to check if the pin is right with two parameters
+const checkPin = (pinNumber, pinNum) => {
+    // if the both pin numbers match and if yes return true
+    if (pinNumber === pinNum) {
+        return true;
+    }
+    // if the pin we guessed is wrong then it will print out this
+    else {
+        console.log('You PIN is wrong !!! Try again !');
+    }
+}
 
-// // we create an arrow function with three parameters of how much cash we want to take out, how much money is in the account, guessed pin number 
-// const cashWith = (cash, moneyInAccount, pin) => {
-//     // we checking if the checkPin function is true
-//     if (checkPin(pin1, pin) == true) {
-//         // if its true then we checking if the amount we want to take out is lower then it is in the bank account 
-//         if (cash <= moneyInAccount) {
-//             // if true then we counting the balance 
-//             let balance = moneyInAccount - cash ;
-//             // printing out with template literals balance and cash
-//             console.log(`The amount of £${cash} was taken out of your account.`);
-//             console.log(`Your balance is £${balance}.`);
-//         }
-//         // if we want to take more money than it is in the account then print out this
-//         else {
-//             console.log('You do not have enough money!');
-//             console.log(`Your balance is £${moneyInAccount}.`);
-//         }
-//     }
-// };
+// we create an arrow function with three parameters of how much cash we want to take out, how much money is in the account, guessed pin number 
+const cashWith = (cash, moneyInAccount, pin) => {
+    // we checking if the checkPin function is true
+    if (checkPin(pin1, pin) == true) {
+        // if its true then we checking if the amount we want to take out is lower then it is in the bank account 
+        if (cash <= moneyInAccount) {
+            // if true then we counting the balance 
+            let balance = moneyInAccount - cash ;
+            // printing out with template literals balance and cash
+            console.log(`The amount of £${cash} was taken out of your account.`);
+            console.log(`Your balance is £${balance}.`);
+        }
+        // if we want to take more money than it is in the account then print out this
+        else {
+            console.log('You do not have enough money!');
+            console.log(`Your balance is £${moneyInAccount}.`);
+        }
+    }
+};
 
-// // we call the function and use our already created variables as the parameters
-// cashWith(cashOut,cashIn,pin2);
-// // we call the function and use integers as arguments
-// cashWith(1000, 2000, 1236);
+// we call the function and use our already created variables as the parameters
+cashWith(cashOut,cashIn,pin2);
+// we call the function and use integers as arguments
+cashWith(1000, 2000, 1236);
